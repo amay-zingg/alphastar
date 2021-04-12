a0b4c9da201a2165f995d266b678ba005589a1e9
amay-zingg <anastasiamay29@gmail.com>
2021-04-12 03:23:48 -0400

    Finished and made responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // * * * * HAMBURGER ICON HOVER EFFECTS
+// makes the top and bottom lines skew
 $('.burgerIcon').hover(function() {
     TweenMax.to('.burgerLine:first-child', 0.2, {x: -10});
     TweenMax.to('.burgerLine:last-child', 0.2, {x: 10});
@@ -11,7 +12,6 @@ function() {
 
 // * * * * NAV MENU TOGGLE - triggered by hamburger icon
 // Note: autoAlpha makes an item that is declared "visibility: hidden" visible instead of opacity controls
-
 var tlmenu = new TimelineMax({paused: true});
 
 tlmenu.to('nav', 0.3, {autoAlpha: 1})
@@ -26,6 +26,34 @@ $('.closeButton').click(function() {
 });
 
 // * * * * HERO
+// Soft entry for text and button
 TweenMax.fromTo('#hero h1', 0.6, { y: 80, opacity: 0}, { y: 0, opacity: 1, delay: 1});
-
 TweenMax.fromTo('#hero .learnMoreButton', 0.6, { y: 80, opacity: 0}, { y: 0, opacity: 1, delay: 1.6, ease: Back.easeOut});
+
+// * * * * FOUR COLUMN SECTION
+// only hover on individual column, get slightly bigger and have a slightly darker box shadow
+$('.fourColItem').hover(function() {
+    TweenMax.to($(this), 0.5, {y: -10, scale: 1.03});
+    TweenMax.to($(this), 0.5, {boxShadow: '0 0 20px rgba(0,0,0,0.36)' });
+},
+function() {
+    TweenMax.to($(this), 0.5, {y: 0, scale: 1});
+    TweenMax.to($(this), 0.5, {boxShadow: '0 0 20px rgba(0,0,0,0.06)' });
+});
+
+// * * * * SERVICES
+// Moves hover text off the photo and on hover animates it into view
+TweenMax.set('.serviceBoxInner', { y: 200, opacity: 0 });
+
+$('.serviceBox').hover(function() {
+    TweenMax.to($(this).find('.serviceBoxInner'), 0.5, { y: 0, opacity: 1, ease: Back.easeOut });
+},
+function(){
+    TweenMax.to($(this).find('.serviceBoxInner'), 0.5, { y: 200, opacity: 0 });
+});
+
+// * * * * COGS
+// has the cogs spinning repeatedly 
+TweenMax.to('.cogLeft', 8, {rotation: 360, repeat: -1, ease:Linear.easeNone });
+TweenMax.to('.cogRight', 8, {rotation: -360, repeat: -1, ease:Linear.easeNone });
+
